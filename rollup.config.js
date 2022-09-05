8ba5537df393d22fb705ab9bcb39fb96fe728cb2
liaozhai <laocan@yandex>
2022-09-05 15:58:13 +0300

    Added GeoJSON

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import scss from 'rollup-plugin-scss';
+import node from 'rollup-plugin-polyfill-node';
+import babel from '@rollup/plugin-babel';
+import alias from 'rollup-plugin-alias';
 
 export default {  
     input: 'src/index.js',  
@@ -9,12 +12,29 @@ export default {
             file: 'public/index.js',
             format: 'iife',
             sourcemap: true,
+            exports: 'auto',
             name: 'App',
+            inlineDynamicImports: true,         
         }
-    ],
-    plugins: [
-        resolve(),
-        commonjs({ extensions: [ '.js', '.msj', ] }),
+    ],    
+    plugins: [        
+        alias({
+            entries: [
+                { find: 'geotiff', replacement: 'node_modules/geotiff/dist-browser/geotiff.js' }
+            ]
+        }),
+        node({
+            include: ['http', 'https', 'url', 'vm']
+        }),        
+        resolve({
+            browser: true,
+            preferBuiltins: false,
+        }),
+        commonjs({
+            extensions: [ '.js', '.msj', ],
+            transformMixedEsModules: true
+        }),
         scss(),
+        babel(),
     ],
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './index.css';
+import 'ol/ol.css';
+import 'elm-pep';
 import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
 import View from 'ol/View';
@@ -6,14 +8,17 @@ import {OSM, Vector as VectorSource} from 'ol/source';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 
 
-window.addEventListener('load', async () => {
-  /*  const vectorSource = new VectorSource({
+window.addEventListener('load', async () => {    
+    const response = await fetch('features.json');
+    const fc = await response.json();
+    
+    const vectorSource = new VectorSource({
         features: new GeoJSON().readFeatures(fc),
     });
 
     const vectorLayer = new VectorLayer({
         source: vectorSource,
-    });*/
+    });
 
     const map = new Map({
         target: 'map',
@@ -27,9 +32,5 @@ window.addEventListener('load', async () => {
             center: [0, 0],
             zoom: 2
         })
-    });
-
-    const response = await fetch('objects.json');
-    const fc = await response.json();
-   // console.log(fc);
+    });   
 });
